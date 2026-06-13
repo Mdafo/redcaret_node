b29caf3a98b77fd10f929da972e6a98834888e7f
Md Afzal <mdafjalkhan.0974@gmail.com>
2026-06-13 18:42:40 +0530

    ok

--- config/db_connection.js
+++ config/db_connection.js
@@ -20,6 +20,13 @@
 
 // module.exports = {sequelize1,sequelize2};
 
+// DB_CONNECTION=mysql
+// DB_HOST=127.0.0.1
+// DB_PORT=3306
+// DB_DATABASE=u683267791_db_redcarat
+// DB_USERNAME=u683267791_admin_redcarat
+// DB_PASSWORD=@Redcarat2026
+
 const { Sequelize } = require('sequelize');
 
 const sequelize1 = new Sequelize(
