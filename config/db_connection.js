8358d7c885fab2727fd9a91ac01d743a3e0bedfe
Md Afzal <mdafjalkhan.0974@gmail.com>
2026-06-13 12:17:51 +0530

    third push

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
 // Configure your database connection
 const sequelize1 = new Sequelize('u683267791_db_redcarat', 'u683267791_admin_redcarat', '@Redcarat2026', {
-  host: '127.0.0.1',    // Your database host
+  host: 'https://auth-db1642.hstgr.io',    // Your database host
   dialect: 'mysql',     // Use the correct dialect (mysql, postgres, sqlite, etc.)
   logging: false,       // Set to true if you want to log SQL queries
   timezone: '+05:30', // Set timezone to IST (India Standard Time)
 });
 
 const sequelize2 = new Sequelize('u683267791_db_redcarat', 'u683267791_admin_redcarat', '@Redcarat2026', {
-  host: '127.0.0.1',    // Your database host
+  host: 'https://auth-db1642.hstgr.io',    // Your database host
   dialect: 'mysql',     // Use the correct dialect (mysql, postgres, sqlite, etc.)
   logging: false,       // Set to true if you want to log SQL queries
   timezone: '+05:30', // Set timezone to IST (India Standard Time)
