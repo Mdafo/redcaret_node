fff16eadfc7b42ec6dfa247715af038e635a65ac
Md Afzal <mdafjalkhan.0974@gmail.com>
2026-06-13 16:35:48 +0530

    ok

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,21 +1,36 @@
-const { Sequelize } = require('sequelize');
-// Configure your database connection
-const sequelize1 = new Sequelize(
-  'u683267791_db_redcarat', 
-  'u683267791_admin_redcarat',
-   '@Redcarat2026',
-  {
-   host: 'auth-db1642.hstgr.io',
-    dialect: 'mysql',
-    logging: false,
-    timezone: '+05:30',
-});
+// const { Sequelize } = require('sequelize');
+// // Configure your database connection
+// const sequelize1 = new Sequelize(
+//   'u683267791_db_redcarat', 
+//   'u683267791_admin_redcarat',
+//    '@Redcarat2026',
+//   {
+//    host: 'auth-db1642.hstgr.io',
+//     dialect: 'mysql',
+//     logging: false,
+//     timezone: '+05:30',
+// });
 
-const sequelize2 = new Sequelize('u683267791_db_redcarat', 'u683267791_admin_redcarat', '@Redcarat2026', {
+// const sequelize2 = new Sequelize('u683267791_db_redcarat', 'u683267791_admin_redcarat', '@Redcarat2026', {
+//     host: 'auth-db1642.hstgr.io',
+//     dialect: 'mysql',
+//     logging: false,
+//     timezone: '+05:30',
+// });
+
+// module.exports = {sequelize1,sequelize2};
+const { Sequelize } = require('sequelize');
+
+const sequelize1 = new Sequelize(
+  'u683267791_db_redcarat',
+  'u683267791_admin_redcarat',
+  '@Redcarat2026',
+  {
     host: 'auth-db1642.hstgr.io',
     dialect: 'mysql',
     logging: false,
     timezone: '+05:30',
-});
+  }
+);
 
-module.exports = {sequelize1,sequelize2};
+module.exports = sequelize1;
