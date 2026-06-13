0df002c1ade850270f0624341fcfa1c254b215ba
Md Afzal <mdafjalkhan.0974@gmail.com>
2026-06-13 12:47:24 +0530

    secound push

--- config/db_connection.js
+++ config/db_connection.js
@@ -5,14 +5,14 @@ const sequelize1 = new Sequelize(
   'u683267791_admin_redcarat',
    '@Redcarat2026',
   {
-   host: 'auth-db1642.hstgr.io',
+   host: '193.203.184.150',
     dialect: 'mysql',
     logging: false,
     timezone: '+05:30',
 });
 
 const sequelize2 = new Sequelize('u683267791_db_redcarat', 'u683267791_admin_redcarat', '@Redcarat2026', {
-    host: 'auth-db1642.hstgr.io',
+    host: '193.203.184.150',
     dialect: 'mysql',
     logging: false,
     timezone: '+05:30',
