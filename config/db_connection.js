268b5a8839b538023faa4f3f6b58af5acf998b09
Md Afzal <mdafjalkhan.0974@gmail.com>
2026-06-13 12:28:51 +0530

    4rthpush

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,17 +1,21 @@
 const { Sequelize } = require('sequelize');
 // Configure your database connection
-const sequelize1 = new Sequelize('u683267791_db_redcarat', 'u683267791_admin_redcarat', '@Redcarat2026', {
-  host: 'https://auth-db1642.hstgr.io',    // Your database host
-  dialect: 'mysql',     // Use the correct dialect (mysql, postgres, sqlite, etc.)
-  logging: false,       // Set to true if you want to log SQL queries
-  timezone: '+05:30', // Set timezone to IST (India Standard Time)
+const sequelize1 = new Sequelize(
+  'u683267791_db_redcarat', 
+  'u683267791_admin_redcarat',
+   '@Redcarat2026',
+  {
+   host: 'auth-db1642.hstgr.io',
+    dialect: 'mysql',
+    logging: false,
+    timezone: '+05:30',
 });
 
 const sequelize2 = new Sequelize('u683267791_db_redcarat', 'u683267791_admin_redcarat', '@Redcarat2026', {
-  host: 'https://auth-db1642.hstgr.io',    // Your database host
-  dialect: 'mysql',     // Use the correct dialect (mysql, postgres, sqlite, etc.)
-  logging: false,       // Set to true if you want to log SQL queries
-  timezone: '+05:30', // Set timezone to IST (India Standard Time)
+    host: 'auth-db1642.hstgr.io',
+    dialect: 'mysql',
+    logging: false,
+    timezone: '+05:30',
 });
 
 module.exports = {sequelize1,sequelize2};
